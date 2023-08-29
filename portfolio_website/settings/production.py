320b9c95f97938df2202e7757f24b0728af5d45e
tarek-berkane <riko.jsk.320@gmail.com>
2023-08-29 19:35:56 +0100

    add production config

--- portfolio_website/settings/production.py
+++ portfolio_website/settings/production.py
@@ -3,62 +3,28 @@ import environ
 from .base import *
 from django.core.exceptions import ImproperlyConfigured
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
 
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+config_file = "/home/mabbit71/config/portfolio"
+env = environ.Env()
+environ.Env.read_env(os.path.join(config_file, ".env"))
 
-# SECRET_KEY_VALUE = os.getenv("SECRET_KEY_VALUE","y04g(6v&((y$29nfs6*fjb5@+di7i*u2c*5vu&zml8fa@7y(^k")
-SECRET_KEY = env("SECRET_KEY_VALUE")
 
-HOST_NAME = env("HOST_NAME")
+SECRET_KEY = env("SECRET_KEY")
+
+HOST_NAME = env("HOST")
 
 ALLOWED_HOSTS = [HOST_NAME]
 
-MIDDLEWARE = [
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    # "django.middleware.cache.UpdateCacheMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    # "django.middleware.cache.FetchFromCacheMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
-]
-
-
-
-
-
-
-DATABASE_PATH = env("DATABASE_PATH")
-
-if not DATABASE_PATH:
-    raise ImproperlyConfigured("DATABASE_PATH should not be empty")
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(DATABASE_PATH, "raw-template_production.db"),
-    }
-}
-
 
 # PUBLIC_FILE_PATH = os.getenv("PUBLIC_STATIC_PATH", "")
-PUBLIC_FILE_PATH = env("PUBLIC_STATIC_PATH")
+PUBLIC_DIR = env("PUBLIC_DIR")
 
-if not PUBLIC_FILE_PATH:
-    raise ImproperlyConfigured("PUBLIC_FILE_PATH should not be empty")
 
 # STATIC
-STATIC_ROOT = os.path.join(PUBLIC_FILE_PATH, "raw-template-static")
+STATIC_ROOT = os.path.join(PUBLIC_DIR, "raw-template-static")
 STATIC_URL = "/raw-template-static/"
 # MEDIA
-MEDIA_ROOT = os.path.join(PUBLIC_FILE_PATH, "raw-template-media")
+MEDIA_ROOT = os.path.join(PUBLIC_DIR, "raw-template-media")
 MEDIA_URL = "/raw-template-media/"
 
 # SSL
@@ -75,9 +41,6 @@ CACHES = {
         },
     }
 }
-
-
-
 
 
 LOGGING = {
@@ -110,8 +73,13 @@ LOGGING = {
 }
 
 
-
-try:
-    from .local import *
-except ImportError:
-    pass
+# settings.py
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": os.path.join(config_file, "my.cnf"),
+            "sql_mode": "traditional",
+        },
+    }
+}
